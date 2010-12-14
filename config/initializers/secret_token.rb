3113d5a18f63846480c2e386272da932a9e23b0e
Stephen Jenkins <sjenkins@cloudcitydigital.com>
2010-12-14 04:03:52 -0500

    startin up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperApp::Application.config.secret_token = 'd1b2f5e394fa23aafd8310d9d5d3b0089151ebb402bcf0ec1e8b9d1cae04b5782ddb95d0aa0ae2afac60070f38704e3f3cdd28b5635ccb48394ac97236008cba'
